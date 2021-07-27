c61ce01e532ccdacc5fa434521a8874817b5a771
TestFirst <39798297+gittadesushil@users.noreply.github.com>
2021-07-27 08:47:11 +0200

    addeing exmaple data files

---
+++ Extras/LoginData.java
@@ -0,0 +1,19 @@
+class LoginData{
+
+    String email;
+    String passwod;
+    String message;
+
+    public static void main(String args[]){
+        LoginData ld1 = new LoginData();
+        ld1.email ="abc@foodstore.com";
+        ld1.passwod = "123456";
+        ld1.message = "";
+
+
+        LoginData ld2 = new LoginData();
+    }
+}
+
+
+
